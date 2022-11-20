cb022ac88c7a4b85a6d31112707080e6c17ae96c
CRMclaughlin <chrisraymusik@gmail.com>
2022-11-20 15:53:43 -0600

    fixed multiple package.json files, added start and dev scripts, added imports to server.js

--- db/index.js
+++ db/index.js
@@ -3,3 +3,12 @@
 // return connection promise
 // connecting to the database
 // using sql to query the database
+
+
+import inquirer from 'inquirer'
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
